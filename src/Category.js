b726c2e4dd6e81af6d6860be0fa2d3cf2452b1be
hirk09 <hirk09@gmail.com>
2020-03-16 17:31:06 +0530

    completed

---
+++ src/Category.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Category extends React.PureComponent {
+  render() {
+    const category = this.props.category;
+    return (
+      <tr>
+        <th colSpan="2">{category}</th>
+      </tr>
+    );
+  }
+}
